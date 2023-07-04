1d689d545203f88005371348a193c478bd8421c7
Kai Tomono <gareibyakueilove@gmail.com>
2023-07-04 21:27:51 +0900

    Apply the Atomic Design to the project

---
+++ gosen-frontend/src/components/Atoms/Header/HeaderUserIcon.tsx
@@ -0,0 +1,12 @@
+import { UserIcon } from '@heroicons/react/24/solid'
+import './HeaderStyle.css';
+
+const HeaderUserIcon = () => {
+    return(
+        <div className="userIcon">
+            <UserIcon type="button"/>
+        </div>
+    );
+}
+
+export default HeaderUserIcon;
